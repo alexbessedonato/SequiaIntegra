19daad6ba998d9ae01a61c5561dde89cc6da7c71
Alex Besse Donato <alexbessedonato03@gmail.com>
2024-10-22 12:58:17 +0200

    Initial commit

---
+++ app/(tabs)/index/index_css.tsx
@@ -0,0 +1,29 @@
+// index_css.js
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  page: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  bgRed: {
+    backgroundColor: "#ffcccc",
+  },
+  bgYellow: {
+    backgroundColor: "#ffe680",
+  },
+  bgGreen: {
+    backgroundColor: "#ccffcc",
+  },
+  bgBlue: {
+    backgroundColor: "#cceeff",
+  },
+  bgPurple: {
+    backgroundColor: "#d9ccff",
+  },
+});
